64dcd97b9c677141b4d30ffcc1e47ca5bbec3f36
monali-2003 <monalikale08@gmail.com>
2026-01-07 11:40:35 +0530

    test node app cicd

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require("http");
+
+// Azure gives dynamic PORT, local uses 3000
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello World from Node.js on Azure App Service!");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
